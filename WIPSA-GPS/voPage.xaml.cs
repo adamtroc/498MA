f0e6ee0b46d6b9e6f6a1297cbd7c211ee8ff295e
lispascal <lispascal@gmail.com>
2013-12-03 22:24:44 -0600

    Added in the image processing "grayscale"
    See title; found grayscale code and was able to change how it displays
    on screen

--- WIPSA-GPS/voPage.xaml.cs
+++ WIPSA-GPS/voPage.xaml.cs
@@ -13,17 +13,26 @@ using Microsoft.Phone.Shell;
 
 using Microsoft.Devices.Sensors; 
 using Microsoft.Devices; 
-using Microsoft.Xna.Framework; 
-using Microsoft.Xna.Framework.Graphics; 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media; 
 using Matrix = Microsoft.Xna.Framework.Matrix; 
 using WIPSA_GPS_PhoneComponent;
+using System.IO;
+
+// Directives 
+using Microsoft.Devices;
+using System.Windows.Media.Imaging;
+using System.Threading; 
+
 
 namespace WIPSA_GPS
 {
     public partial class Page2 : PhoneApplicationPage
     {
         //Direct3DBackground m_d3dBackground = null;
-
+        private int savedCounter = 0;
+        //MediaLibrary library = new MediaLibrary();
         Motion motion;
         PhotoCamera cam;
 
@@ -34,7 +43,15 @@ namespace WIPSA_GPS
         Rectangle viewport;
         Matrix projection;
         Matrix view;
-        Matrix attitude; 
+        Matrix attitude;
+
+        private WriteableBitmap wb;
+        private Thread ARGBFramesThread;
+        private bool pumpARGBFrames; 
+
+        private static ManualResetEvent pauseFramesEvent = new ManualResetEvent(true); 
+
+
 
 
         public Page2()
@@ -62,6 +79,13 @@ namespace WIPSA_GPS
                 cam = new Microsoft.Devices.PhotoCamera(CameraType.Primary);
                 //Event is fired when the PhotoCamera object has been initialized 
                 cam.Initialized += new EventHandler<Microsoft.Devices.CameraOperationCompletedEventArgs>(cam_Initialized);
+
+                // from http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202956(v=vs.105).aspx#BKMK_CreatingTheCameraUI
+                // Event is fired when the capture sequence is complete.
+                cam.CaptureCompleted += new EventHandler<CameraOperationCompletedEventArgs>(cam_CaptureCompleted);
+                // Event is fired when the capture sequence is complete and an image is available.
+                cam.CaptureImageAvailable += new EventHandler<Microsoft.Devices.ContentReadyEventArgs>(cam_CaptureImageAvailable);
+
                 viewfinderBrush.SetSource(cam); 
             }
             else
@@ -102,7 +126,125 @@ namespace WIPSA_GPS
  
             base.OnNavigatedTo(e); 
         }
- 
+
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (cam != null)
+            {
+                // Dispose camera to minimize power consumption and to expedite shutdown.
+                cam.Dispose();
+
+                // Release memory, ensure garbage collection.
+                cam.Initialized -= cam_Initialized;
+                cam.CaptureCompleted -= cam_CaptureCompleted;
+                cam.CaptureImageAvailable -= cam_CaptureImageAvailable;
+            }
+        }
+
+        private void ShutterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (cam != null)
+            {
+                try
+                {
+                    // Start image capture.
+                    cam.CaptureImage();
+                }
+                catch (Exception ex)
+                {
+                    this.Dispatcher.BeginInvoke(delegate()
+                    {
+                        // Cannot capture an image until the previous capture has completed.
+                        txtDebug.Text = ex.Message;
+                    });
+                }
+            }
+        }
+
+        void cam_CaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
+        {
+            // Increments the savedCounter variable used for generating JPEG file names.
+            savedCounter++;
+        }
+
+    // Informs when full resolution photo has been taken, 
+        void cam_CaptureImageAvailable(object sender, Microsoft.Devices.ContentReadyEventArgs e)
+        {
+            string fileName = savedCounter + ".jpg";
+
+            try
+            {   // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Captured image available, saving photo.";
+                });
+
+                // Save photo to the media library camera roll.
+                //library.SavePictureToCameraRoll(fileName, e.ImageStream);
+
+                // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Photo has been dispatched";
+
+                });
+
+                // Set the position of the stream back to start
+                e.ImageStream.Seek(0, SeekOrigin.Begin);
+
+
+                // below uses a memorystream to 
+                MemoryStream ms = new MemoryStream();
+                byte[] buffer = new byte[e.ImageStream.Length];
+                int read;
+                while ((read = e.ImageStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                byte[] byteArray = ms.ToArray();
+
+                process(byteArray);
+
+                /*
+                // Save photo as JPEG to the local folder.
+                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        // Initialize the buffer for 4KB disk pages.
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        // Copy the image to the local folder. 
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+                */
+                // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Photo has been \"saved\" to the local folder.";
+
+                });
+            }
+            finally
+            {
+                // Close image stream
+                e.ImageStream.Close();
+            }
+
+        }
+
+
+        void process(byte[] input)
+        {
+            // do processing here
+            return;
+        }
          //Update UI if initialization succeeds 
         void cam_Initialized(object sender, Microsoft.Devices.CameraOperationCompletedEventArgs e) 
         { 
@@ -115,6 +257,7 @@ namespace WIPSA_GPS
  
             } 
         } 
+
 
         /*private void DrawingSurfaceBackground_Loaded(object sender, RoutedEventArgs e)
         {
@@ -238,7 +381,7 @@ namespace WIPSA_GPS
             } 
  
             // Translate the point before projecting it. 
-            System.Windows.Point p = pointOnScreen; 
+                System.Windows.Point p = pointOnScreen; 
             p.X = LayoutRoot.RenderSize.Width - p.X; 
             p.Y = LayoutRoot.RenderSize.Height - p.Y; 
             p.X *= .5; 
@@ -261,5 +404,105 @@ namespace WIPSA_GPS
             //NameTextBox.Text = ""; 
         }
 
+
+        // ARGB frame pump 
+        void PumpARGBFrames()
+        {
+            // Create capture buffer. 
+            int[] ARGBPx = new int[(int)cam.PreviewResolution.Width * (int)cam.PreviewResolution.Height];
+
+            try
+            {
+                PhotoCamera phCam = (PhotoCamera)cam;
+
+                while (pumpARGBFrames)
+                {
+                    pauseFramesEvent.WaitOne();
+
+                    // Copies the current viewfinder frame into a buffer for further manipulation. 
+                    phCam.GetPreviewBufferArgb32(ARGBPx);
+
+                    // Conversion to grayscale. 
+                    for (int i = 0; i < ARGBPx.Length; i++)
+                    {
+                        ARGBPx[i] = ColorToGray(ARGBPx[i]);
+                    }
+
+                    pauseFramesEvent.Reset();
+                    Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                    {
+                        // Copy to WriteableBitmap. 
+                        ARGBPx.CopyTo(wb.Pixels, 0);
+                        wb.Invalidate();
+
+                        pauseFramesEvent.Set();
+                    });
+                }
+
+            }
+            catch (Exception e)
+            {
+                this.Dispatcher.BeginInvoke(delegate()
+                {
+                    // Display error message. 
+                    txtDebug.Text = e.Message;
+                });
+            }
+        }
+
+        internal int ColorToGray(int color)
+        {
+            int gray = 0;
+
+            int a = color >> 24;
+            int r = (color & 0x00ff0000) >> 16;
+            int g = (color & 0x0000ff00) >> 8;
+            int b = (color & 0x000000ff);
+
+            if ((r == g) && (g == b))
+            {
+                gray = color;
+            }
+            else
+            {
+                // Calculate for the illumination. 
+                // I =(int)(0.109375*R + 0.59375*G + 0.296875*B + 0.5) 
+                int i = (7 * r + 38 * g + 19 * b + 32) >> 6;
+
+                gray = ((a & 0xFF) << 24) | ((i & 0xFF) << 16) | ((i & 0xFF) << 8) | (i & 0xFF);
+            }
+            return gray;
+        } 
+
+        private void GrayOn_Clicked(object sender, RoutedEventArgs e) 
+        { 
+            MainImage.Visibility = Visibility.Visible; 
+            pumpARGBFrames = true; 
+            ARGBFramesThread = new System.Threading.Thread(PumpARGBFrames); 
+ 
+            wb = new WriteableBitmap((int)cam.PreviewResolution.Width, (int)cam.PreviewResolution.Height); 
+            this.MainImage.Source = wb; 
+ 
+            // Start pump. 
+            ARGBFramesThread.Start(); 
+            this.Dispatcher.BeginInvoke(delegate() 
+            { 
+                txtDebug.Text = "ARGB to Grayscale"; 
+            }); 
+        } 
+ 
+        // Stop ARGB to grayscale pump. 
+        private void GrayOff_Clicked(object sender, RoutedEventArgs e) 
+        { 
+            MainImage.Visibility = Visibility.Collapsed; 
+            pumpARGBFrames = false; 
+ 
+            this.Dispatcher.BeginInvoke(delegate() 
+            { 
+                txtDebug.Text = ""; 
+            }); 
+        } 
     }
+
+
 }
